51b921ad751567fac1a12c7e72a66f454c3bdaad
Jim Chen <jchen27@logitech.com>
2020-11-23 21:27:26 +0800

    re-indent input.ts

--- input.ts
+++ input.ts
@@ -32,116 +32,116 @@ module io
         window;
         cpu;
 
-    constructor(document, window, cpu)
-    {
-        this.document = document;
-        this.window = window;
-        this.cpu = cpu;
-        this.mapper = [];
-        this.paused = false;
-        var ref = this;
-        this.document.onkeydown = function(e) {
+        constructor(document, window, cpu)
+        {
+            this.document = document;
+            this.window = window;
+            this.cpu = cpu;
+            this.mapper = [];
+            this.paused = false;
+            var ref = this;
+            this.document.onkeydown = function(e) {
 
-            ref.mapper[e.keyCode] = true;
+                ref.mapper[e.keyCode] = true;
 
-            if ( ref.mapper[ref.P] )
-            {
-                if ( !ref.paused )
+                if ( ref.mapper[ref.P] )
                 {
-                    clearInterval(ref.id)
-                } else
-                {
-                    ref.id = setInterval(ref.window.run, 16);
+                    if ( !ref.paused )
+                    {
+                        clearInterval(ref.id)
+                    } else
+                    {
+                        ref.id = setInterval(ref.window.run, 16);
+                    }
+                    ref.paused = !ref.paused;
                 }
-                ref.paused = !ref.paused;
+            }
+            this.document.onkeyup = function(e) {
+                ref.mapper[e.keyCode] = false;
             }
         }
-        this.document.onkeyup = function(e) {
-            ref.mapper[e.keyCode] = false;
-        }
-    }
 
-    init()
-    {
-        this.IN_PORT2 |= 0; //(0x1 | 0x2);
-        this.IN_PORT2 |= (0x80);
-    }
-
-    update()
-    {
-        this.IN_PORT1 = this.IN_PORT1 & (~(0x1 | 0x2 | 0x4 | 0x10 | 0x20 | 0x40));
-        this.IN_PORT2 = this.IN_PORT2 & (~(0x4 | 0x10 | 0x20 | 0x40));
-
-        if ( this.mapper[this.COIN] ) this.IN_PORT1 |= 0x1;
-        if ( this.mapper[this.MULTIPLAYER] ) this.IN_PORT1 |= 0x2;
-        if ( this.mapper[this.SINGLE] ) this.IN_PORT1 |= 0x4;
-
-        if ( this.mapper[this.LEFT] )
+        init()
         {
-            this.IN_PORT1 |= 0x20;
-            this.IN_PORT2 |= 0x20;
+            this.IN_PORT2 |= 0; //(0x1 | 0x2);
+            this.IN_PORT2 |= (0x80);
         }
 
-        if ( this.mapper[this.RIGHT] )
+        update()
         {
-            this.IN_PORT1 |= 0x40;
-            this.IN_PORT2 |= 0x40;
+            this.IN_PORT1 = this.IN_PORT1 & (~(0x1 | 0x2 | 0x4 | 0x10 | 0x20 | 0x40));
+            this.IN_PORT2 = this.IN_PORT2 & (~(0x4 | 0x10 | 0x20 | 0x40));
+
+            if ( this.mapper[this.COIN] ) this.IN_PORT1 |= 0x1;
+            if ( this.mapper[this.MULTIPLAYER] ) this.IN_PORT1 |= 0x2;
+            if ( this.mapper[this.SINGLE] ) this.IN_PORT1 |= 0x4;
+
+            if ( this.mapper[this.LEFT] )
+            {
+                this.IN_PORT1 |= 0x20;
+                this.IN_PORT2 |= 0x20;
+            }
+
+            if ( this.mapper[this.RIGHT] )
+            {
+                this.IN_PORT1 |= 0x40;
+                this.IN_PORT2 |= 0x40;
+            }
+
+            if ( this.mapper[this.SPACE] )
+            {
+                this.IN_PORT1 |= 0x10;
+                this.IN_PORT2 |= 0x10;
+            }
+
+            if ( this.mapper[this.RESET] )
+            {
+                this.cpu.Reset();
+            }
+
         }
 
-        if ( this.mapper[this.SPACE] )
+        OutPutPort(port, value)
         {
-            this.IN_PORT1 |= 0x10;
-            this.IN_PORT2 |= 0x10;
+            switch  ( port )
+            {
+                case 2:
+                    this.OUT_PORT2 = value;
+                    break;
+                case 3:
+                    this.OUT_PORT3 = value;
+                    break;
+                case 4:
+                    this.OUT_PORT4LO = this.OUT_PORT4HI
+                    this.OUT_PORT4HI = value;
+                    break;
+                case 5:
+                    this.OUT_PORT5 = value;
+                    break;
+            }
         }
 
-        if ( this.mapper[this.RESET] )
+        InputPort(port)
         {
-            this.cpu.Reset();
+            var result = 0;
+            switch ( port )
+            {
+                case 1:
+                    result = this.IN_PORT1;
+                    break;
+                case 2:
+                    result = this.IN_PORT2;
+                    break;
+                case 3:
+                    result = ((((this.OUT_PORT4HI << 8) | this.OUT_PORT4LO) << this.OUT_PORT2) >> 8);
+                    break;
+            }
+            return result
         }
 
-    }
-
-    OutPutPort(port, value)
-    {
-        switch  ( port )
+        set interval(intervalID)
         {
-            case 2:
-                this.OUT_PORT2 = value;
-                break;
-            case 3:
-                this.OUT_PORT3 = value;
-                break;
-            case 4:
-                this.OUT_PORT4LO = this.OUT_PORT4HI
-                this.OUT_PORT4HI = value;
-                break;
-            case 5:
-                this.OUT_PORT5 = value;
-                break;
+           this.id = intervalID;
         }
-    }
-
-    InputPort(port)
-    {
-        var result = 0;
-        switch ( port )
-        {
-            case 1:
-                result = this.IN_PORT1;
-                break;
-            case 2:
-                result = this.IN_PORT2;
-                break;
-            case 3:
-                result = ((((this.OUT_PORT4HI << 8) | this.OUT_PORT4LO) << this.OUT_PORT2) >> 8);
-                break;
-        }
-        return result
-    }
-
-    set interval(intervalID)
-    {
-       this.id = intervalID;
-    }
     }
 }
